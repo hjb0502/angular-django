90a2cd84ed166fe673e2f08724b2884b78150cff
hjb0502 <ad.hjb.gidshd@gmail.com>
2021-11-30 22:32:28 +0900

    angular-django example

---
+++ client/src/app/contact-list/contact-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { APIService } from '../api.service';
+
+@Component({
+  selector: 'app-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css']
+})
+export class ContactListComponent implements OnInit {
+  
+  private contacts: Array<object> = [];
+  constructor(private apiService: APIService) { }
+
+  ngOnInit() {
+    this.getContacts();
+  }
+
+  public getContacts(){
+    this.apiService.getContacts().subscribe((data: Array<object>) => {
+      this.contacts = data;
+      console.log(data);
+    });
+  }
+}
---
+++ server/README.md
@@ -0,0 +1,16 @@
+# A Simple Django CRM (Customer Relationship Management) Project
+
+This project contains Models and Django REST framework API for a simple CRM web application.
+
+## Running
+python manage.py migrate
+python manage.py loaddata db.json
+python manage.py runsever
+
+## Url
+http://localhost:8000
+http://localhost:8000/admin
+
+## Administrator
+username:admin
+password:asdf1234
